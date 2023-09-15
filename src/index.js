ec25f004c388aab612ed2500ba9aec37f70b3fe6
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-09-15 17:27:35 +0900

    react yarn start OK

--- src/index.js
+++ src/index.js
@@ -5,3 +5,17 @@ const foo = require("./foo.js");
 
 bar();
 foo();
+
+
+
+// ＝＝＝＝＝＝＝＝reactの書き方＝＝＝＝＝＝＝＝
+import React from "react";
+import ReactDom from "react-dom";
+
+// コンポーネント
+const App = () => {
+    return <h1>Hello!</h1>;
+};
+
+// 入れる場所
+ReactDom.render(<App />, document.getElementById("app"));
